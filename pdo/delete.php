fb0c1b6f702796c7b67c2f46627f46d4c747ce1c
Adriane Rodrigues <dryrodrigues2009@gmail.com>
2020-01-09 12:54:16 -0300

    delete com pdo

---
+++ pdo/delete.php
@@ -0,0 +1,10 @@
+<?php
+	$conn = new PDO("mysql:dbname=dbphp7; host=localhost", "root", "");
+
+	$stmt = $conn->prepare("DELETE FROM tb_usuarios WHERE idusuario=:id");
+
+	$id = 3;
+
+	$stmt->bindParam(":id", $id);
+	$stmt->execute();
+?>
